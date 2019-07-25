3851df03e0a3f905d70c1f38cf0616460a869162
Ekaterina.Klius <Ekaterina.Klius@billing.ru>
2019-07-25 20:13:47 +0300

    Restart with POM

--- conftest.py
+++ conftest.py
@@ -1,14 +1,22 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default="chrome",
                      help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default="en",
+                     help="Choose language: en or ru")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
+    options = Options()
+    user_language = request.config.getoption("language")
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
+
     browser_name = request.config.getoption("browser_name")
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
